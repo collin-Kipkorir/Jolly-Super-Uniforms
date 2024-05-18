23471b1c7c915b6ddfc162975d4632b2ef80f6ad
collin-Kipkorir <collinkipkorir@gmail.com>
2024-05-18 15:15:24 +0300

    Resolved merge conflicts

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ function hideDropdown() {
     document.getElementById("navbarDropdown").setAttribute("aria-expanded", "false");
     document.getElementById("dropdownMenu").classList.remove("show");
 }
+
 /// Function to show loader and hide content
 function showLoader() {
     document.getElementById('loader').style.display = 'block';
@@ -63,6 +64,7 @@ function hideLoader() {
     document.getElementById('loader').style.display = 'none';
     document.getElementById('main-content').classList.remove('hidden');
 }
+
 // Function to hide the navbar
 function hideNavbar() {
     document.getElementById('navbar').style.display = 'none';
@@ -72,6 +74,7 @@ function hideNavbar() {
 function showNavbar() {
     document.getElementById('navbar').style.display = 'block';
 }
+
 // Function to hide the footer
 function hideFooter() {
     document.getElementById('footer').classList.add('hidden');
@@ -81,6 +84,7 @@ function hideFooter() {
 function showFooter() {
     document.getElementById('footer').classList.remove('hidden');
 }
+
 // Call the function to display loader when the page starts loading
 showLoader();
 hideNavbar();
@@ -105,7 +109,7 @@ function displayProducts() {
                 showNavbar();
                 // Create product card for each product
                 const productCard = document.createElement('div');
-                productCard.classList.add('col-md-3', 'product-card'); // Add product-card class
+                productCard.classList.add('col-6', 'col-md-3', 'product-card'); // Add responsive classes for mobile and desktop view
                 productCard.innerHTML = `
                 <div class="card mb-4 shadow-sm">
                     <div class="d-flex justify-content-center">
@@ -113,25 +117,24 @@ function displayProducts() {
                             <img src="${childData.image}" class="card-img-top" alt="${childData.name}" style="height: 200px; object-fit: contain;"> <!-- Set fixed height and object-fit -->
                         </a>
                     </div>
+                
                     <div class="card-body d-flex flex-column">
                         <div class="card-title-box"><h6 class="card-title">${childData.name}</h6></div>
-                        <!-- <p class="card-text">${childData.description}</p> -->
-                        <div class="d-flex justify-content-between align-items-end mt-auto">
-                            <p class="card-text"><span style="color: red; font-size: 15px; font-weight: bold;">From Kshs. ${childData.price}</span></p> <!-- Apply styling to price text -->
-                            <div class="d-flex justify-content-between align-items-end mt-auto">
-                                <span class="cart-icon">
-                                    <!-- Add event listener to cart icon -->
-                                    <a href="add to cart" style="margin-right: 10px;">
-                                        <img src="images/addcart.png" alt="Add Cart" style="height: 25px;" class="cart-img">
-                                    </a>
-                                </span> <!-- Cart Icon -->
-                                <span class="ml-2 whatsapp-icon">
-                                    <a href="whatsapp://send?phone=+254723914386&text=Hello%20I%20would%20like%20to%20chat">
-                                        <img src="images/whatsapp.png" alt="WhatsApp Logo" style="height: 25px;" class="whatsapp-img">
-                                    </a>
-                                </span> <!-- WhatsApp Logo -->
-                            </div>
-                        </div>
+                        <div> <p class="card-text"><span style="color: red; font-size: 14px; font-weight: bold;">From Kshs. ${childData.price}</span></p></div> <!-- Apply styling to price text -->
+
+                        <div class="d-flex justify-content-end align-items-end mt-auto" style="margin-top: auto;">
+                        <span class="cart-icon">
+                            <!-- Add event listener to cart icon -->
+                            <a href="add to cart" style="margin-right: 10px;">
+                                <img src="images/addcart.png" alt="Add Cart" style="height: 25px;" class="cart-img">
+                            </a>
+                        </span> <!-- Cart Icon -->
+                        <span class="ml-2 whatsapp-icon">
+                            <a href="whatsapp://send?phone=+254723914386&text=Hello%20I%20would%20like%20to%20chat">
+                                <img src="images/whatsapp.png" alt="WhatsApp Logo" style="height: 25px;" class="whatsapp-img">
+                            </a>
+                        </span> <!-- WhatsApp Logo -->
+                    </div>
                     </div>
                 </div>`;
                 productList.appendChild(productCard);
@@ -179,7 +182,6 @@ function logProductID(productId) {
     // Retrieve product details by ID
     getProductDetails(productId);
 }
-
 
 // Call the function to display products when the page loads
 window.onload = () => {
